9511702b6cf75d2c649e35ba38b5562017eff59b
Roman Cardozo <Roman.alejandro.cardozo@gmail.com>
2024-10-08 02:46:53 -0300

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# DEUNA-Challenge
+<div align="center">
+   <h1 style="display: inline-block; vertical-align: middle; font-size: 32px; font-weight: bold;">
+    DEUNA Challenge
+  </h1>
+</div>
 
-#### Online Payment Platform
-* This project aims to develop an online payment platform, which will be an API-based application enabling e-commerce businesses to securely and seamlessly process transactions.
+#### This project aims to develop an online payment platform, which will be an API-based application enabling e-commerce businesses to securely and seamlessly process transactions.
+
+## Prerequisites
+* Docker: Make sure you have Docker installed on your system.
+
+## How to execute 
+1. Clone this repository to your local machine.
+2. Open a terminal in the project root.
+3. Run the following command to build and start the services defined in the ``` docker-compose.yml ``` file:
+```sh
+make build
+```
+This will build the Docker images and start the containers.
+
+## Stopping the Application
+To stop the application and services, press ```Ctrl+C``` in the terminal where docker-compose up is running. Then, run the following command to stop and remove the containers:
+```sh
+make down
+```
 
 ## Project Structure
 ```sh
--- internal/core/models/payment.go
+++ internal/core/models/payment.go
@@ -2,24 +2,22 @@ package models
 
 import "time"
 
-type PaymentStatus string
-
 const (
-	PaymentStatusPending   PaymentStatus = "pending"
-	PaymentStatusCompleted PaymentStatus = "completed"
-	PaymentStatusFailed    PaymentStatus = "failed"
-	PaymentStatusRefunded  PaymentStatus = "refunded"
+	PaymentStatusPending   string = "pending"
+	PaymentStatusCompleted string = "completed"
+	PaymentStatusFailed    string = "failed"
+	PaymentStatusRefunded  string = "refunded"
 )
 
 type Payment struct {
-	ID            string        `json:"id"`
-	CustomerID    string        `json:"customer_id"`
-	MerchantID    string        `json:"merchant_id"`
-	Amount        float64       `json:"amount"`
-	Currency      string        `json:"currency"`
-	Description   string        `json:"description"`
-	Status        PaymentStatus `json:"status"`
-	CreatedAt     time.Time     `json:"created_at"`
-	UpdatedAt     time.Time     `json:"updated_at"`
-	TransactionID string        `json:"transaction_id"`
+	ID            string    `json:"id"`
+	CustomerID    string    `json:"customer_id"`
+	MerchantID    string    `json:"merchant_id"`
+	Amount        float64   `json:"amount"`
+	Currency      string    `json:"currency"`
+	Description   string    `json:"description"`
+	Status        string    `json:"status"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	TransactionID string    `json:"transaction_id"`
 }
